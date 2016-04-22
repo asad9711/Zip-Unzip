a10e05898f076467a26c50b757e1c0dff0f1c08b
Mohd Asad Hussain <asadhussain9711@gmail.com>
2016-04-22 23:32:23 +0530

    Getting Unzipped

--- Unzipp.java
+++ Unzipp.java
@@ -13,56 +13,52 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-class Unzip implements ActionListener
+class Unzipp 
 {
 	// JFrame f;
 	JFileChooser c;
 	String folderName;File f;char first;
-	// JTextField tf;
-	// String[] fileNames;
-	// JButton b;
-	// JLabel jl;
-	Unzip()
+
+	Unzipp()
 	{
-		// f=new JFrame();
-		// tf=new JTextField();
-		// tf.setBounds(40,100,200,30);
-		// b=new JButton("OK");
-		// b.setBounds(60,200,100,40);
-		// b.addActionListener(this);
 		c=new JFileChooser();
-		c.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+		// c.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
 		c.setDialogTitle("choose file");
-		// f.add(c);
-		// f.add(tf);
-		// f.add(b);
-		// f.setSize(600,600);
-		// f.setLayout(null);
-		// f.setVisible(true);
-		// f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		// c.setMultiSelectionEnabled(true);//
+		
 		int x=c.showOpenDialog(null);
+		 // int x=c.showSaveDialog(Unzipp.this);
+
 		if(x==JFileChooser.APPROVE_OPTION)
 		{
 			f=c.getSelectedFile();
 			first=f.toString().charAt(0);
+			// System.out.println(f);
 			folderName=f.getName();
 			System.out.println(f.getName());
-		}
-	}
-	public void actionPerformed(ActionEvent ae)
-	{
+		
+	
+	// public void actionPerformed(ActionEvent ae)
+	// {
 
 		try{
+			System.out.println("inside action");
 			ZipFile zfile=new ZipFile(f);
-			// ZipFile zfile=new ZipFile("st.zip");
+			
 			Enumeration enm=zfile.entries();
+			File newDir=new File(String.valueOf(first));
+				if(!newDir.exists())
+				{
+					newDir.mkdir();
+				}
 			while(enm.hasMoreElements())
 			{
 				ZipEntry entry=(ZipEntry)enm.nextElement();
 				String name=entry.getName();
-				System.out.println("name  "+name);
-				File file=new File(first+":\\"+name);
+				String halfName=name.substring(2);
+				
+				// File file=new File(String.valueOf(first)+":\\"+name);
+				File file=new File(newDir+":\\"+halfName);
+                System.out.println("name  "+name);
 				InputStream is=zfile.getInputStream(entry);
                 FileOutputStream fos=new FileOutputStream(file);
                 int length=0;
@@ -79,8 +75,9 @@ class Unzip implements ActionListener
 			e.printStackTrace();
 		}
 	}
+}
 	public static void main(String s[])
 	{
-		new Unzip();
+		new Unzipp();
 	}
 }
